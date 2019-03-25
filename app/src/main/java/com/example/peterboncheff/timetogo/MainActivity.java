3ae834b6ffb9846d7d435dc2aadced33b45c8307
amy whyte <1402464@rgu.ac.uk>
2019-03-25 20:53:49 +0000

    hi

--- app/src/main/java/com/example/peterboncheff/timetogo/MainActivity.java
+++ app/src/main/java/com/example/peterboncheff/timetogo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.peterboncheff.timetogo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +13,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newPickUp = findViewById(R.id.btn_NewPickUp);
+        newPickUp.setOnClickListener(this);
+
     }
 
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.btn_NewPickUp){
+            Intent launchStartSearchIntent = new Intent (getApplicationContext(), StartSearch.class);
+            startActivity(launchStartSearchIntent);
+        }
+
 
     }
 }
--- app/src/main/java/com/example/peterboncheff/timetogo/StartSearch.java
+++ app/src/main/java/com/example/peterboncheff/timetogo/StartSearch.java
@@ -1,4 +1,14 @@
 package com.example.peterboncheff.timetogo;
 
-public class StartSearch {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchstart);
+    }
 }
